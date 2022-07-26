1ae5411b7f0f1892ffdf518267a6833616ac142e
Ganesh Del Grosso Guzman <gdelgros@margaret.saclay.inria.fr>
2022-07-26 18:45:50 +0200

    debugged data preprocessing

--- Collect.py
+++ Collect.py
@@ -51,8 +51,6 @@ random.seed(seed)
 # Loading Datasets
 
 transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
